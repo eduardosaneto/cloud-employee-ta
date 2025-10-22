c37962c581bff52763c692e9980552302198da72
Eduardo Sá Neto <eneto@mac008810.local>
2025-10-22 13:03:03 -0300

    adjust style

--- src/app/components/dashboard/index.tsx
+++ src/app/components/dashboard/index.tsx
@@ -16,32 +16,27 @@ type DashboardClientUIProps = {
 
 export default function Dashboard({ icp, prospects }: DashboardClientUIProps) {
   return (
-    <div className='grid grid-cols-1 gap-6 md:grid-cols-3'>
-      <div className='space-y-6 md:col-span-2'>
-        <Card>
-          <CardHeader>
-            <h2 className='text-xl font-semibold'>Qualify New Prospects</h2>
-            <p className='text-default-500'>
-              Enter a list of comma-separated domains to qualify.
-            </p>
-          </CardHeader>
-          <CardBody>
-            <QualificationForm icpId={icp.id} />
-          </CardBody>
-        </Card>
-
-        <Card>
-          <CardHeader>
-            <h2 className='text-xl font-semibold'>Qualification History</h2>
-          </CardHeader>
-          <CardBody>
-            <ProspectList prospects={prospects} />
-          </CardBody>
-        </Card>
-      </div>
-      <div className='space-y-6 md:col-span-1'>
-        <IcpDisplay icp={icp} />
-      </div>
-    </div>
+    <section className='flex flex-col gap-8'>
+      <IcpDisplay icp={icp} />
+      <Card>
+        <CardHeader>
+          <h2 className='text-xl font-semibold'>Qualify New Prospects</h2>
+          <p className='text-default-500'>
+            Enter a list of comma-separated domains to qualify against your ICP.
+          </p>
+        </CardHeader>
+        <CardBody>
+          <QualificationForm icpId={icp.id} />
+        </CardBody>
+      </Card>
+      <Card>
+        <CardHeader>
+          <h2 className='text-xl font-semibold'>Qualification History</h2>
+        </CardHeader>
+        <CardBody>
+          <ProspectList prospects={prospects} />
+        </CardBody>
+      </Card>
+    </section>
   )
 }
